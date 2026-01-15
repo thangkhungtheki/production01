93b646c866b21032365485abed57ff9b38f6afc9
thangkhungtheki <thangkhungtheki@gmail.com>
2026-01-15 07:26:17 +0000

    Update from https://github.com/thangkhungtheki/frondend01/commit/88a502171442d5e2114f65b881cfa8f9ba16ba6a

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,30 +4,30 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _4c219d6c = () => interopDefault(import('..\\pages\\baove\\index.vue' /* webpackChunkName: "pages/baove/index" */))
-const _a55ffb08 = () => interopDefault(import('..\\pages\\bep\\index.vue' /* webpackChunkName: "pages/bep/index" */))
-const _bbe50c10 = () => interopDefault(import('..\\pages\\fandb\\index.vue' /* webpackChunkName: "pages/fandb/index" */))
-const _56e1f368 = () => interopDefault(import('..\\pages\\home\\index.vue' /* webpackChunkName: "pages/home/index" */))
-const _e5f84b6e = () => interopDefault(import('..\\pages\\house\\index.vue' /* webpackChunkName: "pages/house/index" */))
-const _d5881a6e = () => interopDefault(import('..\\pages\\ketoan\\index.vue' /* webpackChunkName: "pages/ketoan/index" */))
-const _75760976 = () => interopDefault(import('..\\pages\\lichtiec\\index.vue' /* webpackChunkName: "pages/lichtiec/index" */))
-const _a56a4b40 = () => interopDefault(import('..\\pages\\login\\index.vue' /* webpackChunkName: "pages/login/index" */))
-const _5b1a8003 = () => interopDefault(import('..\\pages\\marketing\\index.vue' /* webpackChunkName: "pages/marketing/index" */))
-const _cc4b16bc = () => interopDefault(import('..\\pages\\nhansu\\index.vue' /* webpackChunkName: "pages/nhansu/index" */))
-const _77eda646 = () => interopDefault(import('..\\pages\\sales\\index.vue' /* webpackChunkName: "pages/sales/index" */))
-const _399409c8 = () => interopDefault(import('..\\pages\\trangtri\\index.vue' /* webpackChunkName: "pages/trangtri/index" */))
-const _47d07840 = () => interopDefault(import('..\\pages\\user\\index.vue' /* webpackChunkName: "pages/user/index" */))
-const _16e802b1 = () => interopDefault(import('..\\pages\\xindex.vue' /* webpackChunkName: "pages/xindex" */))
-const _00acbf13 = () => interopDefault(import('..\\pages\\a4form\\testa4.vue' /* webpackChunkName: "pages/a4form/testa4" */))
-const _dd736830 = () => interopDefault(import('..\\pages\\ketoan\\baocao\\index.vue' /* webpackChunkName: "pages/ketoan/baocao/index" */))
-const _9348c2ca = () => interopDefault(import('..\\pages\\ketoan\\danhsachtaisan\\index.vue' /* webpackChunkName: "pages/ketoan/danhsachtaisan/index" */))
-const _a5f887ea = () => interopDefault(import('..\\pages\\ketoan\\dashboard.vue' /* webpackChunkName: "pages/ketoan/dashboard" */))
-const _0840a3fe = () => interopDefault(import('..\\pages\\ketoan\\hopdong\\index.vue' /* webpackChunkName: "pages/ketoan/hopdong/index" */))
-const _6fd8e42e = () => interopDefault(import('..\\pages\\ketoan\\kiemke\\index.vue' /* webpackChunkName: "pages/ketoan/kiemke/index" */))
-const _0f982a95 = () => interopDefault(import('..\\pages\\ketoan\\report\\index.vue' /* webpackChunkName: "pages/ketoan/report/index" */))
-const _fa1a497a = () => interopDefault(import('..\\pages\\login\\checklogin.vue' /* webpackChunkName: "pages/login/checklogin" */))
-const _2ed52766 = () => interopDefault(import('..\\pages\\template\\table.vue' /* webpackChunkName: "pages/template/table" */))
-const _26ffd25a = () => interopDefault(import('..\\pages\\usr\\setting.vue' /* webpackChunkName: "pages/usr/setting" */))
+const _42eac88e = () => interopDefault(import('..\\pages\\baove\\index.vue' /* webpackChunkName: "pages/baove/index" */))
+const _77deca80 = () => interopDefault(import('..\\pages\\bep\\index.vue' /* webpackChunkName: "pages/bep/index" */))
+const _0b09113c = () => interopDefault(import('..\\pages\\fandb\\index.vue' /* webpackChunkName: "pages/fandb/index" */))
+const _d43d12f0 = () => interopDefault(import('..\\pages\\home\\index.vue' /* webpackChunkName: "pages/home/index" */))
+const _14011ce6 = () => interopDefault(import('..\\pages\\house\\index.vue' /* webpackChunkName: "pages/house/index" */))
+const _4bb34405 = () => interopDefault(import('..\\pages\\ketoan\\index.vue' /* webpackChunkName: "pages/ketoan/index" */))
+const _6b5dfbb2 = () => interopDefault(import('..\\pages\\lichtiec\\index.vue' /* webpackChunkName: "pages/lichtiec/index" */))
+const _164671a4 = () => interopDefault(import('..\\pages\\login\\index.vue' /* webpackChunkName: "pages/login/index" */))
+const _2230d547 = () => interopDefault(import('..\\pages\\marketing\\index.vue' /* webpackChunkName: "pages/marketing/index" */))
+const _5051c5de = () => interopDefault(import('..\\pages\\nhansu\\index.vue' /* webpackChunkName: "pages/nhansu/index" */))
+const _2d04c421 = () => interopDefault(import('..\\pages\\sales\\index.vue' /* webpackChunkName: "pages/sales/index" */))
+const _4dc42550 = () => interopDefault(import('..\\pages\\trangtri\\index.vue' /* webpackChunkName: "pages/trangtri/index" */))
+const _c52b97c8 = () => interopDefault(import('..\\pages\\user\\index.vue' /* webpackChunkName: "pages/user/index" */))
+const _6d275cf5 = () => interopDefault(import('..\\pages\\xindex.vue' /* webpackChunkName: "pages/xindex" */))
+const _191f9557 = () => interopDefault(import('..\\pages\\a4form\\testa4.vue' /* webpackChunkName: "pages/a4form/testa4" */))
+const _47e6a124 = () => interopDefault(import('..\\pages\\ketoan\\baocao\\index.vue' /* webpackChunkName: "pages/ketoan/baocao/index" */))
+const _45e0f7d7 = () => interopDefault(import('..\\pages\\ketoan\\danhsachtaisan\\index.vue' /* webpackChunkName: "pages/ketoan/danhsachtaisan/index" */))
+const _6e8fe172 = () => interopDefault(import('..\\pages\\ketoan\\dashboard.vue' /* webpackChunkName: "pages/ketoan/dashboard" */))
+const _25aaf642 = () => interopDefault(import('..\\pages\\ketoan\\hopdong\\index.vue' /* webpackChunkName: "pages/ketoan/hopdong/index" */))
+const _7eb3e325 = () => interopDefault(import('..\\pages\\ketoan\\kiemke\\index.vue' /* webpackChunkName: "pages/ketoan/kiemke/index" */))
+const _738f005e = () => interopDefault(import('..\\pages\\ketoan\\report\\index.vue' /* webpackChunkName: "pages/ketoan/report/index" */))
+const _c2b1a302 = () => interopDefault(import('..\\pages\\login\\checklogin.vue' /* webpackChunkName: "pages/login/checklogin" */))
+const _430542ee = () => interopDefault(import('..\\pages\\template\\table.vue' /* webpackChunkName: "pages/template/table" */))
+const _557bae17 = () => interopDefault(import('..\\pages\\usr\\setting.vue' /* webpackChunkName: "pages/usr/setting" */))
 
 const emptyFn = () => {}
 
@@ -42,99 +42,99 @@ export const routerOptions = {
 
   routes: [{
     path: "/baove",
-    component: _4c219d6c,
+    component: _42eac88e,
     name: "baove"
   }, {
     path: "/bep",
-    component: _a55ffb08,
+    component: _77deca80,
     name: "bep"
   }, {
     path: "/fandb",
-    component: _bbe50c10,
+    component: _0b09113c,
     name: "fandb"
   }, {
     path: "/home",
-    component: _56e1f368,
+    component: _d43d12f0,
     name: "home"
   }, {
     path: "/house",
-    component: _e5f84b6e,
+    component: _14011ce6,
     name: "house"
   }, {
     path: "/ketoan",
-    component: _d5881a6e,
+    component: _4bb34405,
     name: "ketoan"
   }, {
     path: "/lichtiec",
-    component: _75760976,
+    component: _6b5dfbb2,
     name: "lichtiec"
   }, {
     path: "/login",
-    component: _a56a4b40,
+    component: _164671a4,
     name: "login"
   }, {
     path: "/marketing",
-    component: _5b1a8003,
+    component: _2230d547,
     name: "marketing"
   }, {
     path: "/nhansu",
-    component: _cc4b16bc,
+    component: _5051c5de,
     name: "nhansu"
   }, {
     path: "/sales",
-    component: _77eda646,
+    component: _2d04c421,
     name: "sales"
   }, {
     path: "/trangtri",
-    component: _399409c8,
+    component: _4dc42550,
     name: "trangtri"
   }, {
     path: "/user",
-    component: _47d07840,
+    component: _c52b97c8,
     name: "user"
   }, {
     path: "/xindex",
-    component: _16e802b1,
+    component: _6d275cf5,
     name: "xindex"
   }, {
     path: "/a4form/testa4",
-    component: _00acbf13,
+    component: _191f9557,
     name: "a4form-testa4"
   }, {
     path: "/ketoan/baocao",
-    component: _dd736830,
+    component: _47e6a124,
     name: "ketoan-baocao"
   }, {
     path: "/ketoan/danhsachtaisan",
-    component: _9348c2ca,
+    component: _45e0f7d7,
     name: "ketoan-danhsachtaisan"
   }, {
     path: "/ketoan/dashboard",
-    component: _a5f887ea,
+    component: _6e8fe172,
     name: "ketoan-dashboard"
   }, {
     path: "/ketoan/hopdong",
-    component: _0840a3fe,
+    component: _25aaf642,
     name: "ketoan-hopdong"
   }, {
     path: "/ketoan/kiemke",
-    component: _6fd8e42e,
+    component: _7eb3e325,
     name: "ketoan-kiemke"
   }, {
     path: "/ketoan/report",
-    component: _0f982a95,
+    component: _738f005e,
     name: "ketoan-report"
   }, {
     path: "/login/checklogin",
-    component: _fa1a497a,
+    component: _c2b1a302,
     name: "login-checklogin"
   }, {
     path: "/template/table",
-    component: _2ed52766,
+    component: _430542ee,
     name: "template-table"
   }, {
     path: "/usr/setting",
-    component: _26ffd25a,
+    component: _557bae17,
     name: "usr-setting"
   }],
 
